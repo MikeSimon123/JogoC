2af88a907210cade254a2f274e017077cc7fdab2
MikeSimon123 <mikesimonmikesimon@gmail.com>
2025-11-13 17:31:26 -0300

    Teste de jogo 2D quadrado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,39 @@
 #include <GLFW/glfw3.h>
 
+class meuQuadrado {
+    public:
+        double x,y,w,h;
+        float color[];
+        void resize(double x, double y, double w, double h){
+            this->x = x; this->y = y; this->w = w; this->h = h;
+        };
+        void colorir(double r, double g, double b){
+            this->color[0] = r;
+            this->color[1] = g;
+            this->color[2] = b;
+        };
+        void desenhar(){
+            glColor3f(this->color[0], this->color[1], this->color[2]);
+            glBegin(GL_QUADS);
+                glVertex3f(this->x, this->y, 0);
+                glVertex3f(this->x+w, this->y, 0);
+                glVertex3f(this->x+w, this->y+h, 0);
+                glVertex3f(this->x, this->y+h, 0);
+            glEnd();
+        };
+};
+
+meuQuadrado quadrado1;
+double x = 1.0; double y = 1.0;
+double speed = 0.2;
+
+void desenharCena(){
+    
+    quadrado1.resize(x, y, 1.0, 1.0);
+    quadrado1.colorir(0.3, 0.1, 0.5);
+    quadrado1.desenhar();
+};
+
 int main() {
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(800, 800, "Janela do jogo", NULL, NULL);
@@ -8,32 +42,23 @@ int main() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-5.0, 5.0, -5.0, 5.0, 1.0, -1.0);
-    
+
     while(!glfwWindowShouldClose(window)){
         
         glfwPollEvents();
         if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         }
-        
-        glClearColor(0.1, 0.3, 0.6, 1.0);
+        if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+            x += speed;
+        }
+        if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+            x -= speed;
+        }
+        glClearColor(0.3, 0.3, 0.3, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        desenharCena();
 
-        glColor3f(0.1, 0.1, 0.1);
-        glBegin(GL_TRIANGLES);
-            glVertex3f(0.3, 0.4, 0);
-            glVertex3f(0, 0.1, 0);
-            glVertex3f(0.6, 0.1, 0);
-        glEnd();
-
-        glColor3f(0.4, 0.4, 0.8);
-        glBegin(GL_QUADS);
-            glVertex3f(-0.5, 0.5, 0);
-            glVertex3f(-0.2, 0.5, 0);
-            glVertex3f(-0.2, -0.5, 0);
-            glVertex3f(-0.5, -0.5, 0);
-        glEnd();
-        
         glfwSwapBuffers(window);
         
     }
